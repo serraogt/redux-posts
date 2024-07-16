5825dff19fff79dd49734ae8b16454dbd3b7b971
serraogt <serraogutcen@gmail.com>
2024-07-16 16:40:44 +0300

    ornek-slice-kod

---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+//redux store
+import {configureStore} from "@reduxjs/toolkit"
+import counterReducer from "../features/counter/counterSlice"
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer
+        
+    }
+})
+
+export default store
